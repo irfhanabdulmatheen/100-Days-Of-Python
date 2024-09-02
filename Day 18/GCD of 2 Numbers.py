112e9cf4375ae8cd227e324a043f4f9db5c581eb
Irfhan <irfhan1214@gmail.com>
2024-09-02 14:33:00 +0530

    Create GCD of 2 Numbers.py

---
+++ Day 18/GCD of 2 Numbers.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+    smaller = num2
+else:
+    smaller = num1
+
+gcd = 1
+
+for i in range(1, smaller + 1):
+    if (num1 % i == 0) and (num2 % i == 0):
+        gcd = i  
+print(f"The GCD of {num1} and {num2} is: {gcd}")
