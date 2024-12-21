45a0c041f05eac5eeb510310b13995b52d2d28e1
Irfhan <irfhan1214@gmail.com>
2024-12-21 10:08:39 +0530

    Create Flyod's Triangle.py

---
+++ Day 36/Flyod's Triangle.py
@@ -0,0 +1,9 @@
+def floyd_triangle(rows):
+    num = 1
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print(num, end=" ")
+            num += 1
+        print()
+rows = int(input("Enter the number of rows: "))
+floyd_triangle(rows)
